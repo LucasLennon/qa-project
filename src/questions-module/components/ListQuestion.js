10c25b08d5bcfc713a9a05efff704c239fa8700a
Lucas Lennon <lucasespir@gmail.com>
2021-03-05 02:56:59 -0300

    Add improvements to ListQuestions

--- src/questions-module/components/ListQuestion.js
+++ src/questions-module/components/ListQuestion.js
@@ -1,28 +1,30 @@
 import { arrayOf, func, number, shape, string } from 'prop-types'
 
-import { Flex, Box } from '../../common/styles'
 import { Accordion, Button } from '../../common/components'
+import { Box, ButtonGroup } from '@material-ui/core'
+import { Delete as DeleteIcon, Edit as EditIcon } from '@material-ui/icons'
 
 const ListQuestion = ({ questions, changeQuestion, removeQuestion }) => {
   return (
     <>
       {questions.map((item) => (
-        <Flex flexDirection="column" key={item.id} my="0.5rem">
-          <Accordion header={item.question} value={item.answer} />
-          <Flex>
-            <Button
-              bgColor="error"
-              type="button"
-              mr="0.3rem"
-              onClick={() => removeQuestion({ id: item.id })}
-            >
-              Remove question
-            </Button>
-            <Button type="button" onClick={() => changeQuestion(item)}>
-              Edit question
-            </Button>
-          </Flex>
-        </Flex>
+        <Box display="flex" flexDirection="column" key={item.id} my="0.5rem">
+          <Accordion header={item.question} value={item.answer}>
+            <ButtonGroup disableElevation variant="contained" color="primary">
+              <Button
+                color="secondary"
+                type="button"
+                mr="0.3rem"
+                onClick={() => removeQuestion({ id: item.id })}
+              >
+                <DeleteIcon />
+              </Button>
+              <Button type="button" onClick={() => changeQuestion(item)}>
+                <EditIcon />
+              </Button>
+            </ButtonGroup>
+          </Accordion>
+        </Box>
       ))}
     </>
   )
